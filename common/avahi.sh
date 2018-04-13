21295824414d6367a7be6d336705b4e4cf8bed8b
Slavo N <slavo.ng99@gmail.com>
2018-04-13 09:20:29 +0200

    add avahi and mdns

---
+++ common/avahi.sh
@@ -0,0 +1,21 @@
+yum install -y epel-release # needed for nss-mdns
+yum install -y avahi nss-mdns
+systemctl start avahi-daemon
+systemctl enable avahi-daemon
+
+grep hosts /etc/nsswitch.conf
+sed 's/hosts:      files dns myhostname/hosts: files mdns_minimal [NOTFOUND=return] dns mdns/' -i /etc/nsswitch.conf
+grep hosts /etc/nsswitch.conf
+
+systemctl is-active firewalld
+if [ "$(systemctl is-active firewalld)" == "active" ]
+then
+  firewall-cmd --zone=public --permanent --add-service=mdns
+  systemctl reload firewalld.service
+fi;
+systemctl is-active firewalld
+
+# "systemctl is-active firewalld" returns non-zero exit code
+# but vagrant requires 0 exit code from provisioning
+# hence we use echo here to make sure 0 gets returned back
+echo $?
