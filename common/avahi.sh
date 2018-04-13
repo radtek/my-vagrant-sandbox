0d72e1ef2ce170032486cbef3ac43b6c05ec5686
Slavo N <slavo.ng99@gmail.com>
2018-04-13 10:02:09 +0200

    make sure avahi daemon is started

--- common/avahi.sh
+++ common/avahi.sh
@@ -3,6 +3,7 @@ yum install -y avahi nss-mdns
 systemctl start avahi-daemon
 systemctl enable avahi-daemon
 
+# this is done by "yum install nss-mdns"
 grep hosts /etc/nsswitch.conf
 sed 's/hosts:      files dns myhostname/hosts: files mdns_minimal [NOTFOUND=return] dns mdns/' -i /etc/nsswitch.conf
 grep hosts /etc/nsswitch.conf
@@ -14,8 +15,12 @@ then
   systemctl reload firewalld.service
 fi;
 systemctl is-active firewalld
-
 # "systemctl is-active firewalld" returns non-zero exit code
 # but vagrant requires 0 exit code from provisioning
 # hence we use echo here to make sure 0 gets returned back
 echo $?
+
+systemctl start avahi-daemon
+systemctl enable avahi-daemon
+# should be started and enabled
+systemctl status avahi-daemon
