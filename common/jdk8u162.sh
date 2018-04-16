13d3fea657748a7cf41c006ea35f4ef0a65c19c3
Slavo N <slavo.ng99@gmail.com>
2018-04-16 17:47:00 +0200

    fix bug when jdk was never installed

--- common/jdk8u162.sh
+++ common/jdk8u162.sh
@@ -12,7 +12,7 @@
 # wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
 # http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm -O jdk-8u162-linux-x64.rpm
 #
-if [ ! -z JAVA_HOME ]; then exit 0; fi
+if [ ! -z $JAVA_HOME ]; then exit 0; fi
 
 cp -v /vagrant/downloads/jdk-8u162-linux-x64.rpm .
 if [ ! -f jdk-8u162-linux-x64.rpm ]; then exit 1; fi
